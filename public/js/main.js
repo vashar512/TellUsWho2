298f0b151008564efd1c6b9cd6ac478729083882
David Garcia <dg253@njit.edu>
2014-11-24 10:26:07 -0500

    added bson module to repo

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,9 @@
-$(function() {
-    console.log( "ready!" );
-});
+var jumboHeight = $('.intro-body').outerHeight();
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.intro').css('height', (jumboHeight-scrolled) + 'px');
+}
 
+$(window).scroll(function(e){
+    parallax();
+});
