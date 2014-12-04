f50d423b7868b63dad906049da96a9034fe833a5
Nicole Rutkowski <nrutkowski@northpointdigital.com>
2014-12-04 18:23:10 -0500

    adding facebook functionality

--- index.js
+++ index.js
@@ -34,6 +34,70 @@ passport.deserializeUser(function(obj, done) {
 
 mongoose.connect(process.env.MONGOLAB_URI);
 
+//create user model
+var User = mongoose.model('User', {
+  oauthID: Number,
+  name: String
+});
+
+var Friend = mongoose.model('Friends', {
+  name: String,
+  primaryPicture: String,
+  fbUniqueID: String,
+  fbPicture: String,
+  twitterUniqueID: String,
+  twitterPicture: String,
+  googleUniqueID: String,
+  googlePicture: String,
+  linkedinUniqueID: String,
+  connectionName: String,
+  connectionOauthID: String
+});
+
+passport.use(new FacebookStrategy({
+  //clientID: config.facebook.clientID,
+  //clientSecret: config.facebook.clientSecret,
+  //callbackURL: config.facebook.callbackURL
+  clientID: process.env.FACEBOOK_CLIENT_ID,
+  clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+  callbackURL: process.env.FACEBOOK_CALLBACK_URL
+},
+function(accessToken, refreshToken, profile, done) {
+  console.log("Facebook Profile: " + JSON.stringify(profile));
+  //Getting Friends List for User
+  var fbReq = https.request({
+    hostname: 'graph.facebook.com',
+    method: 'GET',
+    path: '/v2.0/me/taggable_friends?access_token=' + accessToken
+  }, function(fbRes) {
+    var output = '';
+    fbRes.setEncoding('utf8');
+    fbRes.on('data', function(chunk) {
+      output += chunk;
+    });
+
+    fbRes.on('end', function() {
+      fbFriendsList = JSON.parse(output);
+      for(friend in fbFriendsList.data) {
+             addFriends(fbFriendsList, friend, profile, "Facebook");
+      }
+    });
+  });
+
+  fbReq.on('error', function(err) {
+    console.log(">>Error: " + err);
+  });
+
+  fbReq.end();
+  //End Request to get Friends
+
+  checkUser(profile, done);
+  process.nextTick(function() {
+    return done(null, profile);
+  });
+}
+));
+
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(app.router);
